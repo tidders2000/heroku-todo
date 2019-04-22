47c5c933ba5bb92bbd9236919cfe173578ec3a40
Simon Tidman <simon.tid@btinternet.com>
2019-04-22 20:00:23 +0000

    setting updated with ev variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+if os.environ.get("DEVELOPMENT"):
+    development=True
+else:
+    development=False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,10 +26,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+
 SECRET_KEY = 'tt4)+9n+$y%zl%$z$@()@&ky%%til(sgzjuq7^yi4erdsi%$=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+
+DEBUG = development
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
                  os.environ.get('HOSTNAME')]
@@ -76,14 +85,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-DATABASES={"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+if development:
+   DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+    }
+else:
+    
+    DATABASES={"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
